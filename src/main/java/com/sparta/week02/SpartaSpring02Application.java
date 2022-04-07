b4fb539d727914d5e6c901d7bd4824e64e5b6f4f
devjjongs <whdtjr5689@gmail.com>
2022-04-07 17:13:50 +0900

    create new project sparta spring week02
    스파르타코딩 스프링 2주차 프로젝트

---
+++ src/main/java/com/sparta/week02/SpartaSpring02Application.java
@@ -0,0 +1,13 @@
+package com.sparta.week02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaSpring02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaSpring02Application.class, args);
+    }
+
+}
