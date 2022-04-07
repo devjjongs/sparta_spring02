9723a888b096e881c93c6fe1f32177e1dba9c397
devjjongs <whdtjr5689@gmail.com>
2022-04-07 18:21:29 +0900

    jpa 사용 예시
    SpartaSpring02Application.java
    -실제로 프로젝트에서 사용하지 않지만, 이해를 위해 작성된 코드를 통해 jpa가 테이블을 생성하고 저장하는 코드, 저장된 데이터 조회하는 코드 작성
    
    application.properties
    -스프링이 jpa로 작동 할 때 sql을 보여주도록 출력

--- src/main/java/com/sparta/week02/SpartaSpring02Application.java
+++ src/main/java/com/sparta/week02/SpartaSpring02Application.java
@@ -1,7 +1,13 @@
 package com.sparta.week02;
 
+import com.sparta.week02.domain.Course;
+import com.sparta.week02.domain.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpartaSpring02Application {
@@ -10,4 +16,23 @@ public class SpartaSpring02Application {
         SpringApplication.run(SpartaSpring02Application.class, args);
     }
 
+    // Week02Application.java 의 main 함수 아래에 붙여주세요.
+    @Bean
+    public CommandLineRunner demo(CourseRepository repository) {
+        return (args) -> {
+
+            //  jpa 사용법 작성
+            //  생성 및 입력
+            Course course1 = new Course("웹 개발의 봄 Spring", "남병관");
+            repository.save(course1);
+
+            //  조회
+            List<Course> courseList = repository.findAll();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course c = courseList.get(i);
+                System.out.println(c.getTitle());   //  저장된 타이틀
+                System.out.println(c.getTutor());   //  저장된 튜터
+            }
+        };
+    }   //  실제로 프로젝트에서 사용하지 않음, 이해를 위해 작성된 코드
 }
